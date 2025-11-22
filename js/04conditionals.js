be4583ae872b7ddf1e211c8b8143c86dae2b34fa
josue <firej390@gmail.con>
2025-11-22 17:11:02 -0400

    condicionales

---
+++ js/04conditionals.js
@@ -0,0 +1,10 @@
+//condicionales en javascript
+
+//evaluar si es mayor de edad
+
+const age = 12;
+if (age >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor de edad");
+}
