5f7263f6dfb26096e46ba0cbc3ce0275d7e57bb8
josue <firej390@gmail.con>
2025-11-22 17:32:51 -0400

    functions.js

---
+++ js/06functions.js
@@ -0,0 +1,19 @@
+//funciones en javascript
+
+//declaracion de funcion
+function sumar(a, b) {
+    return a + b;
+}
+
+//invocacion de funcion
+console.log(sumar(1, 9));
+
+//funcion flecha
+// const restar = (a, b) => a - b;
+// console.log(restar(2, 1));
+
+// //funcion exprecion
+// const multiplicar = function (a, b) {
+//     return a * b;
+// }
+// console.log(multiplicar(2, 3));
