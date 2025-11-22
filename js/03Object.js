11e3435a8dda5c70ce8b4d805fd58a22ccfd5f15
josue <firej390@gmail.con>
2025-11-22 16:55:57 -0400

    object in js

---
+++ js/03Object.js
@@ -0,0 +1,19 @@
+//objetos en javascript
+const person = {
+    name: "Juan",
+    lastName: "Perez",
+    age: 25,
+    genero: "Masculino",
+    isStudent: true,
+    precio: 10.99,
+    city: "Anytown",
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA",
+        zip: "12345"
+    },
+    hobbies: ["reading", "traveling", "sports"],
+    isStudent: true,
+};
+console.log(person);
