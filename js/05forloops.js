d731b299811249e2485b96373c197a90904d00ef
josue <firej390@gmail.con>
2025-11-22 17:21:25 -0400

    for_loops

---
+++ js/05forloops.js
@@ -0,0 +1,14 @@
+//bucles en javascript
+
+//bucles for
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+//recorrer un array
+const fruits = ["manzana", "banana", "naranja", "uva", "melon"];
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}   
+
+
