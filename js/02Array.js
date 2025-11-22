f3dc323bfee070be94de5bf2a1b59f433b140c7a
josue <firej390@gmail.con>
2025-11-22 16:38:19 -0400

    array in js

---
+++ js/02Array.js
@@ -0,0 +1,8 @@
+// arrays en javascript
+fruits   = ["manzana", "banana", "naranja", "uva", "melon"];
+//console.log(frutas);
+
+fruits.push("piña");
+console.log(fruits);
+
+console.log(fruits[1]);
