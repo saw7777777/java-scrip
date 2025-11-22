fa58775ed6eca2cf3a77366a9adc80c0605b6633
josue <firej390@gmail.con>
2025-11-22 16:24:34 -0400

    first commit

---
+++ js/01variable.js
@@ -0,0 +1,15 @@
+//declaracion de variables
+const name = "Juan";
+const lastName = "Perez";
+let age = 25;
+var genero = "Masculino";
+let isStudent = true;
+let precio = 10.99;
+
+//mostrar variables
+alert(name);
+alert(lastName);
+alert(age);
+alert(genero);
+alert(isStudent);
+alert(precio);
